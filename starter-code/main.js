bf078900a3134b460efb0af929a63409ccc39398
ronyrtan <ron.yr.tan@hotmail.com>
2016-08-27 17:26:13 +1000

    updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,22 @@ var cardOne = 'king';
 var cardTwo = 'king';
 var cardThree = 'queen';
 var cardFour = 'queen';
+
+if (cardTwo === cardFour) {
+	alert('Sorry, try again.');
+}
+else if (cardTwo === cardThree) {
+	alert('Sorry, try again.');
+}
+else if (cardOne === cardFour) {
+	alert('Sorry, try again.');
+}
+else if (cardOne === cardThree) {
+	alert('Sorry, try again.');
+}
+else if (cardOne === cardTwo) {
+	alert('You found a match!');
+}
+else {
+	alert('You found a match!');
+}
