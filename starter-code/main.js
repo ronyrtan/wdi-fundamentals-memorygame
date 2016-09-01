85093d98a1abc93a16ada576265fc2d04b19a130
ronyrtan <ron.yr.tan@hotmail.com>
2016-09-01 13:13:59 +1000

    organised code and dynamic HTML

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,21 +3,34 @@ var cardTwo = 'king';
 var cardThree = 'queen';
 var cardFour = 'queen';
 
-if (cardTwo === cardFour) {
-	alert('Sorry, try again.');
-}
-else if (cardTwo === cardThree) {
-	alert('Sorry, try again.');
-}
-else if (cardOne === cardFour) {
-	alert('Sorry, try again.');
-}
-else if (cardOne === cardThree) {
-	alert('Sorry, try again.');
-}
-else if (cardOne === cardTwo) {
-	alert('You found a match!');
-}
-else {
-	alert('You found a match!');
-}
+//if (cardTwo === cardFour) {
+//	alert('Sorry, try again.');
+//}
+//else if (cardTwo === cardThree) {
+//	alert('Sorry, try again.');
+//}
+//else if (cardOne === cardFour) {
+//	alert('Sorry, try again.');
+//}
+//else if (cardOne === cardThree) {
+//	alert('Sorry, try again.');
+//}
+//else if (cardOne === cardTwo) {
+//	alert('You found a match!');
+//}
+//else {
+//	alert('You found a match!');
+//}
+
+var gameBoard = document.getElementById('game-board');
+var cards = [];
+
+var createBoard = function(){
+	for (var i = 0; i < 4; i++){
+		cards[i] = document.createElement('div')
+		cards[i].className = 'card';
+		gameBoard.appendChild(cards[i]);
+	}
+};
+
+createBoard();
